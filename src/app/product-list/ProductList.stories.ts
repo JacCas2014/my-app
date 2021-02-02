d749abfa4b76528a0201c941e3f2caf56d06f62f
Jacqueline Casey <jacqueline_casey@hotmail.com>
2021-02-02 11:38:18 +0000

    adding first story with default only

---
+++ src/app/product-list/ProductList.stories.ts
@@ -0,0 +1,11 @@
+import { ProductListComponent } from './product-list.component';
+export default {
+  // this is the title for the sidebar of the Storybook app
+  title: 'Product List',
+};
+
+export const Default = () => ({
+    // this is the component itself
+    component: ProductListComponent,
+  });
+
