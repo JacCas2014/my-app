504078f09a7c1d042c5ba5eae89d956ba2f13b02
Jacqueline Casey <jacqueline_casey@hotmail.com>
2021-02-16 20:42:21 +0000

    adding cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 // The root module - AppModule, every Angular app has at least one NgModule class
 @NgModule({
@@ -16,7 +17,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +27,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     // contains routes composed of url path and component to route to
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent},
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ])
   ],
   providers: [],
